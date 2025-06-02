88ddfb08e3966b8a9c25510ec25dd8fb5422389a
schimen <einesimen@gmail.com>
2025-06-02 17:26:00 +0200

    Run keyboard controller as user and clean up services

--- nix/dnd-soundboard-service.nix
+++ nix/dnd-soundboard-service.nix
@@ -1,27 +1,17 @@
 { pkgs, ...}:
 let
   soundboard_package = pkgs.callPackage ./dnd-soundboard.nix {};
+  fifoFile = "/tmp/play_sounds.stdin";
 in
 {
-  systemd = {
-    services.keyboard_controller = {
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${soundboard_package}/bin/keyboard_controller.py";
-        StandardOutput="file:/tmp/play_sounds.stdin";
-      };
-    };
-  };
   systemd.user = {
     sockets.play_sounds = {
-      enable = true;
       description = "Socket for sending commands to play_sounds.service";
       socketConfig = {
-        ListenFIFO = "/tmp/play_sounds.stdin";
+        ListenFIFO = fifoFile;
       };
     };
     services.play_sounds = {
-      enable = true;
       description = "Service that plays sounds when commands are received";
       serviceConfig = {
         Type = "simple";
@@ -30,6 +20,17 @@ in
         StandardOutput = "journal";
         StandardError = "journal";
       };
+    };
+    services.keyboard_controller = {
+      enable = true;
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${soundboard_package}/bin/keyboard_controller.py";
+        StandardOutput = "file:${fifoFile}";
+        StandardError = "journal";
+      };
+      wants = [ "play_sounds.socket" ];
+      wantedBy = [ "default.target" ];
     };
   };
 }
