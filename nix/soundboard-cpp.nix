ebd4acece9fcc59b33948608ed5ce1d21adb34ee
schimen <einesimen@gmail.com>
2026-07-03 22:18:32 +0200

    Update soundboard version

--- nix/soundboard-cpp.nix
+++ nix/soundboard-cpp.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "soundboard-cpp";
-  version = "0.1.0";
+  version = "0.2.0";
   nativeBuildInputs = with pkgs; [ catch2_3 cmake pkg-config ];
   buildInputs = with pkgs; [ argparse libevdev sdbus-cpp sdl3 sdl3-mixer ];
   src = ../.;
