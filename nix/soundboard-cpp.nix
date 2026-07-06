bd20b8bd1fdf088da4703126cb534d55c3e4cfe9
schimen <einesimen@gmail.com>
2026-07-06 12:13:44 +0200

    Add catch2 to buildinputs

--- nix/soundboard-cpp.nix
+++ nix/soundboard-cpp.nix
@@ -3,8 +3,8 @@
 stdenv.mkDerivation {
   name = "soundboard-cpp";
   version = "0.2.0";
-  nativeBuildInputs = with pkgs; [ catch2_3 cmake pkg-config ];
-  buildInputs = with pkgs; [ argparse libevdev sdbus-cpp sdl3 sdl3-mixer ];
+  nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+  buildInputs = with pkgs; [ catch2_3 argparse libevdev sdbus-cpp sdl3 sdl3-mixer ];
   src = ../.;
   cmakeBuildDir = "build-${stdenv.targetPlatform.system}";
   doCheck = true;
